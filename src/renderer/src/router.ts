5e7f027f7f2412d2c1d01c90c0866135fdede771
Clansty <i@gao4.pw>
2022-04-01 17:08:26 +0800

    perf: add routes

--- src/renderer/src/router.ts
+++ src/renderer/src/router.ts
@@ -1,10 +1,16 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 import Wallpaper from './views/Wallpaper.vue';
+import Homework from './views/Homework.vue';
+import Settings from './views/Settings.vue';
+import SloganEdit from './views/SloganEdit.vue';
 
 export default createRouter({
   history: createWebHashHistory(),
   routes: [
     { path: '/', redirect: '/wallpaper' },
     { path: '/wallpaper', component: Wallpaper },
+    { path: '/homework', component: Homework },
+    { path: '/settings', component: Settings },
+    { path: '/sloganEdit', component: SloganEdit },
   ],
 });
