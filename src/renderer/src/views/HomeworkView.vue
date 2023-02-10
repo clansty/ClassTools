18db9bcb5d15abe61a6cf352c7d44b8180070abe
Clansty <i@gao4.pw>
2023-02-10 15:34:56 +0800

    perf: 优化值日生排版

--- src/renderer/src/views/HomeworkView.vue
+++ src/renderer/src/views/HomeworkView.vue
@@ -7,19 +7,14 @@ import { useNow } from '@vueuse/core';
 import homeworks from '../stores/homeworks';
 import { computed } from 'vue';
 import ScheduleDisplay from '../components/ScheduleDisplay.vue';
-import HomeworkDisplay from '../components/HomeworkDisplay.vue';
 import getTime from '../utils/getTime';
 
 const now = useNow();
 
-// 需要把作业平均分配到设定好数量到组里
-const groupedHomeworks = computed(() => {
-  // 筛选有内容的
-  const entries = Object.entries(homeworks.value.homeworks)
-    .filter(([subject, content]) => content);
-  const result: { [subject: string]: string }[] = Array(settings.value.homeworkViewerCols);
+const distribute = <T>(entries: [string, T][], columns: number) => {
+  const result: { [subject: string]: T }[] = Array(columns);
   // 这里不能用 fill，因为不然会全都变成同一个对象，只能一个个赋值
-  for (let i = 0; i < settings.value.homeworkViewerCols; i++) {
+  for (let i = 0; i < columns; i++) {
     result[i] = {};
   }
   let current = 0;
@@ -27,10 +22,19 @@ const groupedHomeworks = computed(() => {
   for (const [subject, content] of entries) {
     result[current][subject] = content;
     current++;
-    if (current === settings.value.homeworkViewerCols) current = 0;
+    if (current === columns) current = 0;
   }
   return result;
-});
+};
+// 需要把作业平均分配到设定好数量到组里
+const groupedHomeworks = computed(() => distribute(
+  Object.entries(homeworks.value.homeworks)
+    .filter(([subject, content]) => content),
+  settings.value.homeworkViewerCols));
+const groupedDuty = computed(() => distribute(
+  Object.entries(settings.value.duty[dutyWeekday.value])
+    .filter(([subject, content]) => content),
+  settings.value.homeworkViewerCols));
 // 现在的时间大于设定的时间，把这个分出来因为下面的文字要显示 今日/明日
 const showTomorrowSchedule = computed(() =>
   getTime(now.value) > getTime(settings.value.showTomorrowScheduleAfter));
@@ -40,20 +44,12 @@ const scheduleWeekday = computed(() =>
   ).getDay());
 const showTomorrowDuty = computed(() =>
   getTime(now.value) > getTime(settings.value.showTomorrowDutyAfter));
-const dutyTitle = computed(() => // 不想在下面嵌套太多三目表达式
-  showTomorrowDuty.value ? '明日值日生' : '今日值日生');
 const dutyWeekday = computed(() =>
   new Date(now.value.getTime() +
     (showTomorrowDuty.value ? 1000 * 60 * 60 * 24 : 0),
   ).getDay());
 const isScheduleShown = computed(() => // 要确保明天有课
   settings.value.showSchedule && settings.value.schedule.some(session => session[scheduleWeekday.value]));
-const isDutyShown = computed(() =>
-  settings.value.showDuty &&
-  Object.entries(settings.value.duty[dutyWeekday.value])
-    .some(([type, student]) => student));
-
-console.log(Object.entries(settings.value.duty[now.value.getDay()]));
 
 const editHomework = () => window.ipcRenderer.send('window:open', 'homeworkEdit');
 const minimize = () => window.ipcRenderer.send('window:minimize');
@@ -84,8 +80,10 @@ const containerStyle = computed(() => ({
         </n-gi>
 
         <!-- 值日生显示，在作业下方 -->
-        <n-gi v-for="(names, type) in settings.duty[dutyWeekday]" style="display: grid; gap: 16px; grid-template-columns: 100%">
-          <n-card :title="type"
+        <n-gi v-for="i in settings.homeworkViewerCols"
+              style="display: grid; gap: 16px; grid-template-columns: 100%">
+          <n-card v-for="(names, type) in groupedDuty[i-1]"
+                  :title="type"
                   style="--n-font-size: 1em; --n-title-font-size: 1.2em"
                   content-style="word-wrap: break-word; white-space: pre-wrap; font-size: 1em">
             {{ names.replace(' ', '，') }}
