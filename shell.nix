5e2d3e985c51727d2507fd485f3e904fe9eebc6b
clansty <i@gao4.pw>
2023-10-09 02:25:39 +0800

    use pnpm

--- shell.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs ? import <nixpkgs> {} }:
-  pkgs.mkShell {
-    buildInputs = with pkgs; [
-      yarn
-      nodejs-16_x
-    ];
-
-    ELECTRON_OVERRIDE_DIST_PATH = "${pkgs.electron_22}/bin";
-}
